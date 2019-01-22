72eb49109f126fa7013f62644ed8da398f276786
SINAN COSKUN <EXT01D4575@turkcell.entp.tgc>
2019-01-22 23:13:13 +0300

    hello with circleCI

---
+++ src/main/java/com/circleci/CircleciApplication.java
@@ -0,0 +1,14 @@
+package com.circleci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleciApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/circleci/CircleciApplicationTests.java
@@ -0,0 +1,17 @@
+package com.circleci;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CircleciApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
